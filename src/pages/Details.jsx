4eeb0ee774b7beb54575315e9ae6ed268bd9d538
williter895-bit <williter895@gmail.com>
2026-07-07 19:22:53 +0300

    Update Details.jsx

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './details.css';
+import './Details.css';
 
 export default function Details() {
   const navigate = useNavigate();
