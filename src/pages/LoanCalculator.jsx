3202b99c1a9b51de2c0c6bb2a26700330bc63a8c
williter895-bit <williter895@gmail.com>
2026-07-07 19:20:30 +0300

    Update LoanCalculator.jsx

--- src/pages/LoanCalculator.jsx
+++ src/pages/LoanCalculator.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './loancalculator.css';
+import './LoanLalculator.css';
 
 export default function LoanCalculator() {
   const navigate = useNavigate();
